104d747120b6b183cd9416b467e1902e71edd6b5
Oleh <vdovenko.oleg.1983@gmail.com>
2025-11-26 23:23:31 +0200

    fix mistakes

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,13 +2,16 @@
 import axios from 'axios';
 
 export function getImagesByQuery(query) {
-  return axios.get('https://pixabay.com/api/', {
-    params: {
-      key: '53377567-48fe25733f93d655972fbcb7e',
-      q: query,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-    },
-  });
+  return axios
+    .get('https://pixabay.com/api/', {
+      params: {
+        key: '53377567-48fe25733f93d655972fbcb7e',
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    })
+    .then(response => response.data)
+    .catch(error => error);
 }
--- src/main.js
+++ src/main.js
@@ -3,41 +3,61 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 import { getImagesByQuery } from './js/pixabay-api';
-import { createGallery } from './js/render-functions';
-import { clearGallery } from './js/render-functions';
-import { showLoader } from './js/render-functions';
-import { hideLoader } from './js/render-functions';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions';
 
 const formElem = document.querySelector('.form');
 
 formElem.addEventListener('submit', e => {
   e.preventDefault();
   clearGallery();
-  showLoader();
+
   const formData = new FormData(formElem);
   const formValues = Object.fromEntries(formData.entries());
+  if (formValues['search-text'].trim() === '') {
+    iziToast.show({
+      message: `Please enter a search query!`,
+      messageColor: 'white',
+      messageSize: '20',
+      backgroundColor: 'red',
+      position: 'center',
+      timeout: 2000,
+    });
+    formElem.reset();
+    return;
+  }
+  showLoader();
   getImagesByQuery(formValues['search-text'].trim())
-    .then(response => {
-      if (
-        response.data.hits.length === 0 ||
-        formValues['search-text'].trim() === ''
-      ) {
+    .then(data => {
+      if (data.hits.length === 0) {
         iziToast.show({
           message: `Sorry, there are no images matching your search query. Please try again!`,
-          messageColor: 'white',
+          messageColor: 'black',
           messageSize: '20',
-          backgroundColor: 'red',
+          backgroundColor: 'yellow',
           position: 'center',
           timeout: 2000,
         });
         hideLoader();
       } else {
-        createGallery(response.data.hits);
-        hideLoader();
+        createGallery(data.hits);
       }
+      hideLoader();
     })
     .catch(error => {
-      console.log('false');
+      hideLoader();
+      iziToast.show({
+        message: `Your request is fail!`,
+        messageColor: 'white',
+        messageSize: '20',
+        backgroundColor: 'red',
+        position: 'center',
+        timeout: 2000,
+      });
     });
   formElem.reset();
 });
